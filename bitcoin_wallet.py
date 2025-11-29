c4929eee2dc26a54b4dbd72873ec1af885bc9432
antoniolaikauf <antonio.laikauf00@gmail.com>
2025-11-29 10:44:05 +0100

    start

---
+++ bitcoin_wallet.py
@@ -0,0 +1,102 @@
+import hashlib
+import ecdsa
+import base58
+from termcolor import colored
+from Crypto.Hash import RIPEMD160
+import secrets
+
+# print(secrets.token_bytes(16).hex())  generate new bits entropy 
+
+# entropy_bits= 'cbc448d8bc1e9f4a36be10b2e06efd29'
+entropy_bits= secrets.token_bytes(16).hex()
+bits=''.join([bin(int(x,16))[2:].zfill(4) for x in entropy_bits])
+
+entropy_bits=bytes.fromhex(entropy_bits)
+sha256=hashlib.sha256(entropy_bits).hexdigest()
+checksum=bin(int(sha256[:1],16))[2:].zfill(4) # first 4 bits of sha256
+hex_string=bits + checksum
+array_bits_words=[hex_string[i : i + 11] for i in range(0,len(hex_string), 11)] #chunks of 11 bits  
+seed_phrase=''
+
+with open('words.txt', mode='r') as f:
+    words=f.readlines()
+
+    for x in range(12): # seed prhase 12
+        extracted_bits= array_bits_words[x]
+        index_word=int(extracted_bits,2)
+        seed_phrase+= ' ' +  words[index_word].rstrip()
+
+    seed_phrase=bytes(seed_phrase,'utf-8')
+
+# Parameters
+hash_name = 'sha512'  # The hash algorithm to use
+salt = b'mnemonic' # + passphrase  # mnemonic è una stringa che è sempre permanente 
+iterations = 2048  # Number of iterations
+dklen = 64  # Length of the derived key (512 bits)
+
+def key_stretching_function(n,s_p,s,i,dk): # bits seed
+    bit_seed= hashlib.pbkdf2_hmac(n, s_p, s, i, dk).hex()
+    return bytes(bit_seed,'utf-8')
+
+bits_seed_512=key_stretching_function(hash_name,seed_phrase,salt,iterations,dklen)
+
+# Derive the key e master chain
+private_key_master_chain= hashlib.pbkdf2_hmac(hash_name, bits_seed_512, b'Bitcoin seed', dklen).hex() #one interaction
+
+private_key, master_chain=private_key_master_chain[:64], private_key_master_chain[64:]
+print(f"private key: {colored(private_key, 'red')}, master chain: {colored(master_chain, 'blue')}")
+
+# a = 'ef' + private_key
+# two_sha256=hashlib.sha256(hashlib.sha256(bytes.fromhex(a)).digest()).hexdigest()[:8]
+# print(two_sha256)
+# print(f'dffffffffffffffffffffffffffff  {base58.b58encode(bytes.fromhex(a + two_sha256))}' )
+
+private_key_bytes = bytes.fromhex(private_key)
+sk = ecdsa.SigningKey.from_string(private_key_bytes, curve=ecdsa.SECP256k1) # eliptic curve 
+vk = sk.verifying_key
+public_key = (b'\04' + vk.to_string()).hex() # 04 | x | y cordinate on SECP256k1
+public_key_y, public_key_x = hex(vk.pubkey.point.y())[2:], hex(vk.pubkey.point.x())[2:] # separate cordinate  
+
+def prefisso(x, y, p=''): # prefix of hex cordinate curve 
+    if int(y[-1], 16) % 2 == 0:  p='02'
+    else: p='03'
+    return {'public_key_x':p + x, 'public_key_y':y}
+
+'''
+the public key compressed is the cordinate of the axis X on the eliptic curve 
+'''
+
+pair_key=prefisso(public_key_x, public_key_y)
+print(f"chiave pubblica: {public_key}\nchiave pubblica compressa:{pair_key['public_key_x']}\npunto x in SECP256k1: {pair_key['public_key_x']}\npunto y in SECP256k1: {pair_key['public_key_y']}")
+
+class Bitcoin_address():
+    def __init__(self, public_key):
+        self.publick_key=public_key
+
+    def sha_256(self): # first process of the two hash 32 bits 
+        sha256_kp=hashlib.sha256()
+        # sha256_kp.update(bytes(self.publick_key,'utf-8'))
+        sha256_kp.update(bytes.fromhex(self.publick_key))
+        sha256_kp=sha256_kp.hexdigest()
+        return sha256_kp
+    
+    def ripend_160(self,value): # second process of the two hash 20 bits 
+        h=RIPEMD160.new()
+        h.update(bytes.fromhex(value))
+        h=h.hexdigest()
+        return h # payload 
+    
+    def base58encoding(self,payload):# chiave pubblica
+        version = '6f' # per testnet la versione è 6f invece per il mainet è 00
+        data = version + payload
+        #two sha256
+        two_sha256=hashlib.sha256(hashlib.sha256(bytes.fromhex(data)).digest()).hexdigest()
+
+        checksum = two_sha256[:8] # checksum
+        data+= checksum # 25 bytes
+        address=base58.b58encode(bytes.fromhex(data))
+        return address
+
+address=Bitcoin_address(pair_key['public_key_x'])
+payload=address.ripend_160(address.sha_256())
+print(f"ADDRESS: {colored(address.base58encoding(payload),'red')}")
--- transaction.c
+++ transaction.c
@@ -1,6 +1,35 @@
 #include <stdio.h>
+#include <string.h>
+
+/*
+private key: 7ce1e22db4aad7f5acd824274fb7b667ee043335704086d14b4304d7773915cb, master chain: b933a96ad026819afb1c0df85639c767f4b36152ca50e260da9af9b4ed74d975
+chiave pubblica: 048798252534f2fbeb6cb3d08a504042babae188736588ec6e8f8011452707d368c18bfcd0a7bdb4c53f73c9fccdbfa9ba8129f9e95eff368213b6e00b99e87ae2
+chiave pubblica compressa:028798252534f2fbeb6cb3d08a504042babae188736588ec6e8f8011452707d368
+punto x in SECP256k1: 028798252534f2fbeb6cb3d08a504042babae188736588ec6e8f8011452707d368
+punto y in SECP256k1: c18bfcd0a7bdb4c53f73c9fccdbfa9ba8129f9e95eff368213b6e00b99e87ae2
+ADDRESS: b'mwc6H8nvFqjNJypGSSsQxf6iZEGNHsW6ia'
+
+3dba85d707442dd2bcb89934688e69ff63a91bd275d9780b50c0b4a3d9cdfaa7
+spedire soldi a n2jNQhRTHz377juqkNCATovwGAnjmssDNt
+*/
+
+struct Wallet
+{
+    char address[34];
+    int amount;
+};
 
 int main()
 {
+    struct Wallet user_1;
+    struct Wallet user_2;
+
+    user_1.amount = 50;
+    user_2.amount = 0;
+
+    // da questo address mwc6H8nvFqjNJypGSSsQxf6iZEGNHsW6ia --> a questo address n2jNQhRTHz377juqkNCATovwGAnjmssDNt
+    strcpy(user_1.address, "mwc6H8nvFqjNJypGSSsQxf6iZEGNHsW6ia");
+    strcpy(user_2.address, "n2jNQhRTHz377juqkNCATovwGAnjmssDNt");
+
     printf("hello\n");
 }
