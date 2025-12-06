57eccd1d48a249a99d2461d2f40d378c131a36ff
antoniolaikauf <antonio.laikauf00@gmail.com>
2025-12-06 11:20:30 +0100

    transaction

--- transaction.c
+++ transaction.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 #include "helper.c"
 
 // https://learnmeabitcoin.com/technical/transaction/
@@ -21,13 +22,13 @@ spedire soldi a n2jNQhRTHz377juqkNCATovwGAnjmssDNt
 
 #define PUBLICKEY "048798252534f2fbeb6cb3d08a504042babae188736588ec6e8f8011452707d368c18bfcd0a7bdb4c53f73c9fccdbfa9ba8129f9e95eff368213b6e00b99e87ae2"
 #define PRIVATEKEY "7ce1e22db4aad7f5acd824274fb7b667ee043335704086d14b4304d7773915cb"
-#define VERSION "0x02000000"
-#define MARKER "0x00"                                                                    // si utilizza solo quando si è richiesto di sbloccar degli input da wallet di tipo P2WPKH, P2WSH, P2TR
-#define FLAG "0x01"                                                                      // flag per transazioni witness
-#define INPUT_COUNT "0x01"                                                               // quantità di input della transaction
-#define TRANSACTION "0x3dba85d707442dd2bcb89934688e69ff63a91bd275d9780b50c0b4a3d9cdfaa7" // transazione da cui prendere i BTC
-#define SEQUENCE "0xfdffffff"                                                            // quando si vuole spendere i BTC
-#define VOUT '0x01000000'                                                                // indice dell'output che vuoi spendere, in una transazione si possono avere più output, e si possono decidere quale spendere
+#define VERSION "02000000"
+#define MARKER "00"                                                                    // si utilizza solo quando si è richiesto di sbloccar degli input da wallet di tipo P2WPKH, P2WSH, P2TR
+#define FLAG "01"                                                                      // flag per transazioni witness
+#define INPUT_COUNT "01"                                                               // quantità di input della transaction
+#define TRANSACTION "3dba85d707442dd2bcb89934688e69ff63a91bd275d9780b50c0b4a3d9cdfaa7" // transazione da cui prendere i BTC
+#define SEQUENCE "fdffffff"                                                            // quando si vuole spendere i BTC
+#define VOUT "01000000"                                                                // indice dell'output che vuoi spendere, in una transazione si possono avere più output, e si possono decidere quale spendere
 
 char transaction[200];
 
@@ -35,9 +36,10 @@ struct Inputs
 {
     char Txid[32]; // transazione da dove vuoi prelevare i BTC
     char Vout[8];  // quale output della transazione vuoi spendere (perchè ogni transazione può avere più di un output)
-    char ScriptSigSize[2];
+    char ScriptSigSize[3];
     char Scriptsig[150];
-    char Sequence[8];
+    char Sequence[9];
+    const char *InputTransaction[300];
 };
 
 struct Outputs
@@ -56,10 +58,16 @@ struct Wallet
     char privateKey[64];
 };
 
+void TransactionCreation(struct Inputs *input)
+{
+    *input->InputTransaction = "r";
+};
+
 int main()
 {
     struct Wallet user_1;
     struct Wallet user_2;
+    struct Inputs inputTransaction;
 
     int amount_to_transfer = 50;
     user_1.amount = 50;
@@ -71,9 +79,17 @@ int main()
     strcpy(user_1.privateKey, PRIVATEKEY);
     strcpy(user_2.address, "n2jNQhRTHz377juqkNCATovwGAnjmssDNt");
 
+    strcpy(inputTransaction.Txid, TRANSACTION);
+    strcpy(inputTransaction.Vout, VOUT);
+    strcpy(inputTransaction.Sequence, SEQUENCE);
+    sprintf(inputTransaction.Scriptsig, "%s%s%s", OP_PUSHBYTES_65, user_1.publicKey, OP_CHECKSIG);
+    snprintf(inputTransaction.ScriptSigSize, sizeof(inputTransaction.ScriptSigSize), "%ld", strlen(inputTransaction.Scriptsig));
+
+    TransactionCreation(&inputTransaction);
     *user_1.type_address = type_adress(user_1.address);
     *user_2.type_address = type_adress(user_2.address);
     printf("address1 --> %s, type_address -->  %s\n", user_1.address, *user_1.type_address);
     printf("address2 --> %s, type_address -->  %s\n", user_2.address, *user_2.type_address);
-    printf("hello %s\n", VERSION);
+    printf("scriptsig --> %s\n", inputTransaction.Scriptsig);
+    printf("scriptsig --> %s\n", *inputTransaction.InputTransaction);
 }
